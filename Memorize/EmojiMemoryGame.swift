8e302977ceebfecd05307fcfd8779b3ddd8ada80
Дмитрий Легостаев <legostaevofficial@yandex.ru>
2024-01-07 14:44:35 +0300

    Class 3. Formatting

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -10,7 +10,6 @@ import SwiftUI
 class EmojiMemoryGame {
     private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
-    
     static func createMemoryGame() -> MemoryGame<String> {
         let emojis: Array<String> = ["🥒", "🍅"];
         
